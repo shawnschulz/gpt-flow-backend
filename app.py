9e1b95c805ac6ae1474fc1f0cc143c725691642e
shawnschulz <shawnschulz@berkeley.edu>
2023-06-02 15:13:04 -0700

    added post testing

--- app.py
+++ app.py
@@ -1,14 +1,14 @@
 from flask import Flask, send_from_directory
 from flask_restful import Api, Resource, reqparse
-#from flask_cors import CORS #comment this on deployment
+from flask_cors import CORS #comment this on deployment
 from api.HelloApiHandler import HelloApiHandler
 
 app = Flask(__name__, static_url_path='', static_folder='frontend/build')
-#CORS(app) #comment on deploy
+CORS(app) #comment on deploy
 api = Api(app)
 
 @app.route("/", defaults={'path':''})
 def serve(path):
-    return send_from_directory(app.static_folder, 'index.html') #<- p sure this doesn't work for flask hookup
+    return send_from_directory(app.static_folder, 'index.html') #<- p sure this doesn't work for react hookup
 
-api.add_resource(HelloApiHandler, '/flask/hello')
+api.add_resource(HelloApiHandler, '/flask/prompts')
